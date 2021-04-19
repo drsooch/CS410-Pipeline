b57ecc89cd9a6175bac0f151a95a83976fd481fd
lukemvu <luke.m.vu@gmail.com>
2021-04-19 13:08:04 -0400

    Removed dependency on JSONBender

--- luke/python-test/transform_utilities.py
+++ luke/python-test/transform_utilities.py
@@ -1,5 +1,7 @@
 # transform_utilities.py
 #
+# Unique keys are required within the nested source data model.
+#
 # Internal Data Model
 # {
 #     'id': '',
@@ -26,45 +28,85 @@
 
 import json
 import uuid
-from jsonbender import bend, K, S
 
 
 def transform(input_string, mapping):
     """
     Takes a JSON formatted string and mapping.
     """
-    input_data = json.loads(input_string, strict=False)
-    result = bend(mapping, input_data)
-    result['id'] = str(uuid.uuid4())
-    return json.dumps(result, indent=2)
+    data = json.loads(input_string, strict=False)
+    output_data = build_data(data, mapping)
+    output_data = add_uuid(output_data)
+    return json.dumps(output_data, indent=2)
+
+def add_uuid(data):
+    """
+    Adds a UUID to the Python dictionary.
+    """
+    data['id']= str(uuid.uuid4())
+    return data
+
+
+def build_data(data, mapping):
+    """
+    Returns the transformed Python dictionary from a key mapping.
+    """
+    output = dict()
+    for key1, key2 in mapping.items():
+        if isinstance(key2, dict):
+            output[key1] = build_data(data, key2)
+        elif key2 != '':
+            output[key1] = find_by_key(key2, data)
+    return output
+
+
+def find_by_key(target, data):
+    """
+    Returns the value of the target key from a nested Python dictionary.
+    """
+    for key, value in data.items():
+        if isinstance(value, dict):
+            return find_by_key(target, value)
+        elif key==target:
+            return value
+    return ''
+
 
 def main():
+    """
+    Example run of the function with a JSON document and mapping.
+    """
+
+    # Court document JSON file
     file = open('4582560.json', 'r')
     data_string = file.read()
 
+    # Key mapping from internal data model to source model
     MAPPING = {
-        'id': K(''),
-        'metadata': {
-            'api_id': S('id'),
-            'api_resource_url': S('resource_uri'),
-            'api_date_modified': S('date_modified'),
-            'court_from': K(''),
-            'citations': S('opinions_cited'),
-            'created': S('date_created'),
-            'modified': S('date_modified'),
-            'pages': S('page_count'),
-            'document_type': S('type'),
-            'document_date': K(''),
-            'precedence': K(''),
-            'case_type': K('')
+        'id': '',
+        'metadata':
+        {
+            'api_id': 'id',
+            'api_resource_url': 'resource_uri',
+            'api_date_modified': 'date_modified',
+            'court_from': '',
+            'citations': 'opinions_cited',
+            'created': 'date_created',
+            'modified': 'date_modified',
+            'pages': 'page_count',
+            'document_type': 'type',
+            'document_date': '',
+            'precedence': '',
+            'case_type': ''
         },
-        'content': {
-            'document_title': K(''),
-            'document_text': S('plain_text')
+        'content':
+        {
+            'document_title': '',
+            'document_text': 'plain_text'
         }
     }
 
-    # JSON formatted string
+    # JSON formatted string output
     output = transform(data_string, MAPPING)
     print(output)
 
