f3c8bb2d1a801e1121314f9a0b739ca65ccb346e
Nick Suchecki <nicksuchecki@gmail.com>
2021-04-27 23:49:32 -0400

    Restructured code to allow for imports and exports through this file

--- src/pipeline.py
+++ src/pipeline.py
@@ -1,11 +1,5 @@
 #
 #
 # This file will hold any top level exports. Preferably from external directory
-#
-# EXAMPLE:
-#
-# from .external import CourtListenerDAG, GenericDAG
-#
 
-from external.court_listener import court_listener
-from external.generic_dag import generic_dag
+from external import *
